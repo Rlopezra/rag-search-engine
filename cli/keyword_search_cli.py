6e576171b47658778642a42709e54a0bae2ad979
Rlopezra <rlopezra.com>
2026-08-07 23:47:20 -0700

    Added the InvertedIndex, helper functions, and refactored code

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,31 +1,125 @@
 import argparse
+from collections import defaultdict, Counter
 import json
 import string
 from nltk.stem import PorterStemmer
+import pickle
+import sys
+import os
+
+with open("data/stopwords.txt", "r", encoding="utf-8") as file:
+        stopwords = file.read().lower().splitlines()
+
+def load_movies():
+    with open("data/movies.json", "r", encoding="utf-8") as file:
+        movie_data = json.load(file)
+    return movie_data
+
+def tokenize_text(text: str) -> list[str]:
+    stemmer = PorterStemmer()
+    translator = str.maketrans('', '', string.punctuation)
+    strip_text =text.translate(translator).lower().split()
+    non_empt_tokens = [stemmer.stem(w) for w in strip_text if w and w not in stopwords]
+    return non_empt_tokens
+
+def tok_single_term(term):
+    token = tokenize_text(term)
+    if len(token) > 1:
+        raise ValueError("Not a single token")
+     
+    return token[0]
+
+
+class InvertedIndex:
+
+    def __init__(self):
+        self.index = defaultdict(set)
+        self.docmap = {}
+        self.term_frequencies = defaultdict(Counter)
+
+    def __add_document(self, doc_id, text):
+        tokens = tokenize_text(text)
+
+        self.term_frequencies[doc_id].update(tokens)
+
+        for tok in tokens:
+            self.index[tok].add(doc_id)
+
+    
+    def get_documents(self, term):
+        terms_doc_ids = self.index.get(term, set())
+        sorted_results = sorted(terms_doc_ids)
+
+        return sorted_results
+    
+    def get_tf(self, doc_id, term):
+        return self.term_frequencies[doc_id][term]
+    
+    def build(self):
+        movie_data = load_movies()
+
+        for movie in movie_data["movies"]:
+            join_title_desc = f"{movie['title']} {movie['description']}"
+            self.docmap[movie["id"]] = movie
+            self.__add_document(movie["id"], join_title_desc)
+
+    def save(self):
+        os.makedirs("cache", exist_ok=True)
+
+        with open("cache/index.pkl", "wb") as index_file:
+            pickle.dump(self.index, index_file)
+
+        with open("cache/docmap.pkl", "wb") as docmap_file:
+            pickle.dump(self.docmap, docmap_file)
+
+        with open("cache/term_frequencies.pkl", "wb") as counter_file:
+            pickle.dump(self.term_frequencies, counter_file)
+
+    def load(self):
+        #Index loading
+        if not os.path.exists("cache/index.pkl"):
+            raise FileNotFoundError(f"Error: Index file does not exist.")
+        
+        with open("cache/index.pkl", "rb") as file:
+            self.index = pickle.load(file)
+
+        #docmap loading
+        if not os.path.exists("cache/docmap.pkl"):
+            raise FileNotFoundError(f"Error: DocMap file doesn't exist.")
+
+        with open("cache/docmap.pkl", "rb") as file:
+            self.docmap = pickle.load(file)
+
+        #counter loading
+        if not os.path.exists("cache/term_frequencies.pkl"):
+            raise FileNotFoundError(f"Error: Term Frequency file doesn't exist.")
+
+        with open("cache/term_frequencies.pkl", "rb") as file:
+            self.term_frequencies = pickle.load(file)
+
+
+def build_command():
+    index = InvertedIndex()
+    index.build()
+    index.save()
+
 
 def main() -> None:
-    stemmer = PorterStemmer()
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     search_parser = subparsers.add_parser("search", help="Search movies using keywords")
     search_parser.add_argument("query", type=str, help="Search query")
 
-    args = parser.parse_args()
-    translator = str.maketrans('', '', string.punctuation)
+    #build parser
+    subparsers.add_parser("build", help="Build the inverted index and save it to disk")
 
-    with open("data/movies.json", "r", encoding="utf-8") as file:
-        movie_data = json.load(file)
-    result_list = []
-
-    with open("data/stopwords.txt", "r", encoding="utf-8") as file:
-        stopwords = file.read()
-    stopwords = stopwords.translate(translator).lower()
-    stopwords = stopwords.splitlines()
+    #term parser
+    tf_parser = subparsers.add_parser("tf", help="Search the term frequency for in a doc")
+    tf_parser.add_argument("doc_id", type=int, help="doc id to search in")
+    tf_parser.add_argument("term", type=str, help="Term to get its term frequency")
 
 
-    search_term_strip = args.query.translate(translator).lower().split()
-    non_empt_tokens = [stemmer.stem(w) for w in search_term_strip if w and w not in stopwords]
 
     def match_substring(movie_title, list1, list2):
         for item in list1:
@@ -34,19 +128,34 @@ def main() -> None:
                     return movie_title
         return None
     
-    for movie in movie_data["movies"]:
-        stripped_movie = movie["title"].translate(translator).lower().split()
-        stripped_movie_clean = [stemmer.stem(word) for word in stripped_movie if word and word not in stopwords]
-        result = match_substring(movie["title"],non_empt_tokens,  stripped_movie_clean)
-        if result:
-            result_list.append(result)
-
+    
+    args = parser.parse_args()
+    index = InvertedIndex()
     match args.command:
         case "search":
+            try:
+                index.load()
+            except Exception as e:
+                print(e)
+                sys.exit()
+            search_term_strip = tokenize_text(args.query)
             print(f"Searching for: {args.query}")
-            for i, item in enumerate(result_list[:5], start=1):
-                print(f"{i}. {item}")
+
+            for search_token in search_term_strip:
+                results = index.get_documents(search_token)
+                for i, item in enumerate(results[:5], start=1):
+                    print(f"{i}. {index.docmap[item]}")
             pass
+        case "build":
+            build_command()
+        case "tf":
+            try:
+                index.load()
+            except Exception as e:
+                print(e)
+                sys.exit()
+            token = tok_single_term(args.term)
+            print(index.get_tf(args.doc_id, token))
         case _:
             parser.print_help()
 
